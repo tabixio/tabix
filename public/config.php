28bb5fe50a03027f73c842f6ad8876df7cf1f26e
fesh <atom_lipetsk@list.ru>
2016-06-21 17:12:17 +0300

    Убраны разрешенные типы запросов

--- public/config.php
+++ public/config.php
@@ -4,11 +4,3 @@ $configClickhouse = [
     "host" => "http://druid.stry.in",
     "port" => "8123",
 ];
-
-$allowPatch = [
-    "/api/login",
-    "/api/logout",
-    "/api/user",
-    "/api/query"
-];
-
