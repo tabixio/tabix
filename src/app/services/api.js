fcfc528c9585a3916415c97f62c1fdce200191cc
Igor <isublimity@gmail.com>
2016-10-03 10:13:43 +0300

    error in limits

--- src/app/services/api.js
+++ src/app/services/api.js
@@ -73,13 +73,24 @@
 				url += '&'+extend_settings;
 
 			}
+			function appendTransform(defaults, transform) {
+
+				// We can't guarantee that the default transformation is an array
+				defaults = angular.isArray(defaults) ? defaults : [defaults];
+
+				// Append the new transformation to the defaults
+				return defaults.concat(transform);
+			}
 
 			var req = {
 				method: 'GET',
 				url: url,
 				headers: {
 					'Content-Type': undefined
-				}
+				},
+				transformResponse: appendTransform($http.defaults.transformResponse, function(value) {
+					return (value);
+				})
 			};
 
 			if (format) {
