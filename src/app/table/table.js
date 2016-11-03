9a1a9d49d7b1bc00e58ea93e6a9f1fd1d3ee64a5
funtom48 <ivan40in48@gmail.com>
2016-11-03 17:04:55 +0300

    ~ tableContainer + tableRoute

--- src/app/table/table.js
+++ src/app/table/table.js
@@ -1,106 +1,20 @@
 (( angular, smi2 ) => {
-	'use strict';
+    'use strict';
 
-	angular.module( smi2.app.name ).controller( 'TableController', TableController );
-	TableController.$inject = [ '$scope', '$rootScope', 'API', 'ThemeService', '$stateParams' ];
+    angular.module( smi2.app.name ).controller( 'TableController', TableController );
+    TableController.$inject = [ '$scope', '$stateParams' ];
 
-	/**
+    /**
 	 * @ngdoc controller
 	 * @name smi2.controller:TableController
 	 * @description Контроллер страницы 1 таблицы БД
 	 */
-	function TableController( $scope, $rootScope, API, ThemeService, $stateParams ) {
+    function TableController( $scope, $stateParams ) {
+        let { dbName, tableName } = $stateParams;
+        $scope.vars = {
+            dbName,
+            tableName
+        };
 
-		$scope.vars = {
-			columns: {},
-            currentDatabase: $stateParams.dbName || $rootScope.currentDatabase,
-            currentTable: $stateParams.tableName || $rootScope.currentTable,
-			data: null,
-			grid: null,
-			limit: 100,
-			offset: 0,
-			statistics: {},
-			scrollConfig: {
-				autoHideScrollbar: false,
-				theme: ThemeService.isDark( )
-					? 'light'
-					: 'dark',
-				scrollButtons: {
-					enable: false
-				},
-				scrollInertia: 100,
-				advanced: {
-					updateOnContentResize: true
-				}
-			}
-		};
-
-        if (!$stateParams.dbName && !$stateParams.tableName) {
-            $rootScope.$watch('currentTable', (val, prev) => {
-                if (val && val !== prev) {
-                    $scope.vars.currentTable = $rootScope.currentTable;
-                    $scope.vars.currentDatabase = $rootScope.currentDatabase;
-                    $scope.init();
-                }
-            });
-        }
-
-		/**
-		 * Загрузка данных
-		 */
-		$scope.load = ( ) => {
-			$scope.vars.data = -1;
-			API.query( `
-			    select * 
-			    from ${$scope.vars.currentDatabase}.${$scope.vars.currentTable}
-			    limit ${$scope.vars.offset}, ${$scope.vars.limit}
-            `).then( function ( data ) {
-				$scope.vars.data = API.dataToHtml( data );
-			}, function ( response ) {
-				console.error( 'Ошибка ' + response );
-			});
-		};
-
-		$scope.init = ( ) => {
-			/**
-			* Запрос полей таблицы
-			*/
-			API.query( 'describe table ' + $scope.vars.currentDatabase + '.' + $scope.vars.currentTable ).then( data => $scope.vars.columns = data );
-
-			/**
-			* Запрос статистики по таблице
-			*/
-			API.query( 'SELECT ' +
-				'	table, ' +
-				'	formatReadableSize(sum(bytes)) as size, ' +
-				'	sum(bytes) as sizeBytes, ' +
-				'	min(min_date) as minDate, ' +
-				'	max(max_date) as maxDate ' +
-				'FROM  ' +
-				'	system.parts ' +
-				'WHERE ' +
-				'	database = \'' + $scope.vars.currentDatabase + '\' AND ' + '	( ' + '		table = \'' + $scope.vars.currentTable + '\' OR ' + '		table = \'' + $scope.vars.currentTable + '_sharded\'' + '    ) ' + 'GROUP BY ' + '    table ' ).then(response => $scope.vars.statistics = (response && response.data.length && response.data[0]) || {});
-
-			$scope.load( );
-		};
-
-		/**
-		 * Шаг вперед по данным
-		 */
-		$scope.loadNext = ( ) => {
-			$scope.vars.offset += $scope.vars.limit;
-			$scope.load( );
-		};
-
-		/**
-		 * Шаг назад по данным
-		 */
-		$scope.loadPrev = ( ) => {
-			if ( $scope.vars.offset > 0 ) {
-				$scope.vars.offset -= $scope.vars.limit;
-				$scope.load( );
-			}
-		};
-		$scope.init( );
-	}
+    }
 })( angular, smi2 );
