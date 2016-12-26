906007e752b05a9f091971f3fdc53a7ed8ed8ad0
atomlipetsk <atom.lipetsk@gmail.com>
2016-12-26 16:03:40 +0300

    Faster watch and script rebuild

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -9,22 +9,17 @@ var browserSync = require('browser-sync');
 var $ = require('gulp-load-plugins')();
 
 
-gulp.task('scripts-reload', function() {
-	return buildScripts()
-		.pipe(browserSync.stream());
+gulp.task('scripts-reload', ['scripts'], function() {
+        browserSync.reload();
 });
 
 gulp.task('scripts', function() {
-	return buildScripts();
-});
-
-function buildScripts() {
-	return gulp.src(path.join(conf.paths.src, '/app/**/*.js'))
-		.pipe($.eslint())
-		.pipe($.eslint.format())
+    return gulp.src(path.join(conf.paths.src, '/app/**/*.js'))
+        .pipe($.eslint())
+        .pipe($.eslint.format())
         .pipe($.babel({
             presets: ['es2015', 'stage-0']
         }))
         .on('error', conf.errorHandler('babel'))
-		.pipe($.size())
-};
+        .pipe($.size());
+});
