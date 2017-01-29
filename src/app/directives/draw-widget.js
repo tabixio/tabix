adc4bf7b9795ad8ed0b063b0c7089ddb55b403df
Igor <isublimity@gmail.com>
2017-01-30 01:23:11 +0300

    fx

--- src/app/directives/draw-widget.js
+++ src/app/directives/draw-widget.js
@@ -12,39 +12,39 @@
         };
     }]);
 
-console.log("<<<<widgetDraw>>>>>");
+
     function buildLinkFunc($window) {
 
         return function (scope, ele, attrs) {
             console.warn('buildLinkFunc',scope);
-
-            var chart, options;
-            chart = echarts.init(ele[0], 'macarons');
-
             //
-            createChart(scope.widget);
-
+            // var chart, options;
+            // chart = echarts.init(ele[0], 'macarons');
             //
-            function createChart(options) {
-                if (!options) return;
-
-                chart.setOption(options);
-                // scope.$emit('create', chart);
-
-                angular.element($window).bind('resize', function(){
-                    chart.resize();
-                });
-
-            }
-
-            // при изменении в виджете
-            scope.$watch('widget', function (newVal, oldVal) {
-                if (angular.equals(newVal, oldVal)) return;
-                createChart(widget);
-            })
+            // //
+            // createChart(scope.widget);
+            //
+            // //
+            // function createChart(options) {
+            //     if (!options) return;
+            //
+            //     chart.setOption(options);
+            //     // scope.$emit('create', chart);
+            //
+            //     angular.element($window).bind('resize', function(){
+            //         chart.resize();
+            //     });
+            //
+            // }
+            //
+            // // при изменении в виджете
+            // scope.$watch('widget', function (newVal, oldVal) {
+            //     if (angular.equals(newVal, oldVal)) return;
+            //     createChart(widget);
+            // })
 
 
         };
     }
 
-});
+})(angular, smi2);
