27a9f96a4e97f41f21762db32b867dd55fa44f5a
Igor <isublimity@gmail.com>
2017-03-11 11:48:51 +0300

    treemap

--- src/app/draw/draw-echarts-treemap.js
+++ src/app/draw/draw-echarts-treemap.js
@@ -9,32 +9,80 @@
 class DrawEchartsTreemap extends DrawEcharts {
 
 
+    getLevelOption() {
+        return [
+            {
+                itemStyle: {
+                    normal: {
+                        borderWidth: 0,
+                        gapWidth: 5
+                    }
+                }
+            },
+            {
+                itemStyle: {
+                    normal: {
+                        gapWidth: 1
+                    }
+                }
+            },
+            {
+                colorSaturation: [0.35, 0.5],
+                itemStyle: {
+                    normal: {
+                        gapWidth: 1,
+                        borderColorSaturation: 0.6
+                    }
+                }
+            }
+        ];
+    };
 
     create() {
+
+
+        // Если это код не JS попробуем получить обьект
+        let drw=this.getDrawCommandObject();
+
+        let sets={
+            path        :'',
+            value       :'value',
+            source      :'source',
+            target      :'target'
+        };
+
+        if (drw)
+        {
+            sets=Object.assign(sets,drw);
+        }
+
+        let treeData=[];
+
 
         let o= {
             tooltip: {
                 trigger: 'item'
-            },
-            series: [
-                {
-                    type:'treemap',
-                    visibleMin: 300,
-                    label: {
-                        show: true,
-                        formatter: '{b}'
-                    },
-                    itemStyle: {
-                        normal: {
-                            borderColor: '#fff'
-                        }
-                    },
-                    levels: getLevelOption(),
-                    data: diskData
-                }
-            ]
+            }
+
         };
 
+        o.series=[
+            {
+                type:'treemap',
+                visibleMin: 300,
+                label: {
+                    show: true,
+                    formatter: '{b}'
+                },
+                itemStyle: {
+                    normal: {
+                        borderColor: '#fff'
+                    }
+                },
+                levels: this.getLevelOption(),
+                data: treeData
+            }
+        ];
 
         this.options=Object.assign(o,this.options);
         return true;
