9a8003a15b46a1e0055002b18ca43ee365a8cfc7
Igor <isublimity@gmail.com>
2017-03-21 10:59:39 +0300

    loader

---
+++ loader/master.js
@@ -0,0 +1,26 @@
+var cssId = 'TabixCss';
+var tabix_build = "201703";
+
+if (!document.getElementById(cssId))
+{
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'https://ui.tabix.io/styles/app.css?build='+tabix_build;
+    link.media = 'all';
+    head.appendChild(link);
+
+
+    var sc = document.createElement('script');
+    sc.type = 'text/javascript';
+    sc.async = false;// !not ASYNC!
+    sc.src = 'https://ui.tabix.io/scripts/app.js?build='+tabix_build;
+    sc.charset = 'utf-8';
+    head.appendChild(sc);
+
+
+}
+
+
