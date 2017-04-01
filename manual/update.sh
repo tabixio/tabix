621e26b83b6ac7f71db644e945d967a95d9d4808
Igor <isublimity@gmail.com>
2017-04-01 11:22:48 +0300

    update.sh

--- manual/update.sh
+++ manual/update.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-git pull
-/usr/local/bin/mkdocs build
+if [[ `git status --porcelain` ]]; then
+  git pull
+  /usr/local/bin/mkdocs build
+else
+  # no changes
+fi
+
+
