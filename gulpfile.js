c724ccd24ed81ef24f097263869d2a80fb521ed5
igor <isublimity@gmail.com>
2017-10-10 21:51:17 +0300

    Light`s f1

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,6 @@
 
 var gulp = require('gulp');
 var glob = require('glob');
-
 /**
  *  This will load all js or coffee files in the gulp directory
  *  in order to load all gulp tasks
@@ -16,7 +15,6 @@ var glob = require('glob');
 glob.sync('./gulp/**/*.js').forEach(function (file) {
     require(file);
 });
-
 /**
  *  Default task clean temporaries directories and launch the
  *  main optimization build task
