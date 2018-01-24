753e41fe43ce2a101b3cd909f30dfe03cbdad1aa
Igor <isublimity@gmail.com>
2018-01-24 19:39:04 +0300

    TS

--- tabix.server/vendor/composer/autoload_files.php
+++ tabix.server/vendor/composer/autoload_files.php
@@ -7,4 +7,5 @@ $baseDir = dirname($vendorDir);
 
 return array(
     '3a37ebac017bc098e9a86b35401e7a68' => $vendorDir . '/mongodb/mongodb/src/functions.php',
+    '98794879b9f3bba010855e1c65c9d901' => $vendorDir . '/smi2/phpclickhouse/include.php',
 );
--- tabix.server/vendor/composer/autoload_real.php
+++ tabix.server/vendor/composer/autoload_real.php
@@ -23,35 +23,24 @@ class ComposerAutoloaderInitd153e2a5442461bfce4df9d4785be4d8
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitd153e2a5442461bfce4df9d4785be4d8', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
-        if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+        $map = require __DIR__ . '/autoload_namespaces.php';
+        foreach ($map as $namespace => $path) {
+            $loader->set($namespace, $path);
+        }
 
-            call_user_func(\Composer\Autoload\ComposerStaticInitd153e2a5442461bfce4df9d4785be4d8::getInitializer($loader));
-        } else {
-            $map = require __DIR__ . '/autoload_namespaces.php';
-            foreach ($map as $namespace => $path) {
-                $loader->set($namespace, $path);
-            }
+        $map = require __DIR__ . '/autoload_psr4.php';
+        foreach ($map as $namespace => $path) {
+            $loader->setPsr4($namespace, $path);
+        }
 
-            $map = require __DIR__ . '/autoload_psr4.php';
-            foreach ($map as $namespace => $path) {
-                $loader->setPsr4($namespace, $path);
-            }
-
-            $classMap = require __DIR__ . '/autoload_classmap.php';
-            if ($classMap) {
-                $loader->addClassMap($classMap);
-            }
+        $classMap = require __DIR__ . '/autoload_classmap.php';
+        if ($classMap) {
+            $loader->addClassMap($classMap);
         }
 
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInitd153e2a5442461bfce4df9d4785be4d8::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
+        $includeFiles = require __DIR__ . '/autoload_files.php';
         foreach ($includeFiles as $fileIdentifier => $file) {
             composerRequired153e2a5442461bfce4df9d4785be4d8($fileIdentifier, $file);
         }
