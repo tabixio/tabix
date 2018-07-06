eb4e3d1e8e88869335f29d9ecfb708262b67a1b5
Igor <isublimity@gmail.com>
2018-07-06 11:45:06 +0300

    FocusStyleManager

--- src/index.jsx
+++ src/index.jsx
@@ -9,9 +9,12 @@ import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 import 'react-flexbox-grid/dist/react-flexbox-grid.css';
 import './styles/index.less';
+import { FocusStyleManager } from "@blueprintjs/core";
 
 const history = createHistory();
 const store = configureStore(history);
+// Enable behavior which hides focus styles during mouse interaction.
+FocusStyleManager.onlyShowFocusOnTabs();
 
 ReactDOM.render(
     <Provider store={store}>
