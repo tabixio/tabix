5cc71b34ae2eb0100adae1b2e2b581e1c07489cf
igor <isublimity@gmail.com>
2018-07-16 17:58:01 +0300

    webpack:Chunk-commons

--- webpack.config.js
+++ webpack.config.js
@@ -98,7 +98,15 @@ const build = async () => {
                 test: /[\\/]node_modules[\\/]|[\\/]libs\/ramda[\\/]|Vendor[\\/]/,
                 name: 'vendors',
                 chunks: 'all'
+                // priority: 10,
+                // enforce: true
             }
+            // ,commons: {
+            //     chunks: 'initial',
+            //     minChunks: 2,
+            //     maxInitialRequests: 5, // The default limit is too small to showcase the effect
+            //     minSize: 0 // This is example is too small to create commons chunks
+            // },
         }
     };
 
