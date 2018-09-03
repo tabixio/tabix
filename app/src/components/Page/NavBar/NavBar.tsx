63b4215bfdcecd529b519e7085b94af015950bc9
Vladimir Zhukov <vzh.inbox@gmail.com>
2018-09-03 17:08:05 +0600

    Add sign out

--- app/src/components/Page/NavBar/NavBar.tsx
+++ app/src/components/Page/NavBar/NavBar.tsx
@@ -1,18 +1,50 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Layout } from 'antd';
+import { Link, RouteComponentProps, withRouter } from 'react-router-dom';
+import { Layout, Button } from 'antd';
 import { Flex } from 'reflexy';
+import { Authorized } from '@vzh/react-auth';
 import { routePaths } from 'routes';
 import css from './NavBar.css';
 
-export default function NavBar() {
-  return (
-    <Flex component={Layout.Header} alignItems="center">
-      <Link to={routePaths.home.path} className={css['brand']} />
+interface Props extends RouteComponentProps<any> {}
 
-      <Flex grow justifyContent="flex-end">
+class NavBar extends React.Component<Props> {
+  private signOut = () => {
+    const { history } = this.props;
+    history.push(routePaths.signOut.path);
+  };
+
+  render() {
+    return (
+      <Flex component={Layout.Header} alignItems="center">
         <Link to={routePaths.home.path} className={css['brand']} />
+
+        <Flex grow justifyContent="flex-end">
+          <Authorized role="">
+            <Button
+              type="primary"
+              icon="logout"
+              size="large"
+              className={css.item}
+              onClick={this.signOut}
+            >
+              Sign Out
+            </Button>
+            {/* <Menu theme="dark" mode="horizontal" onClick={this.signOut}>
+              <Menu.Item>
+                <Icon type="logout" theme="outlined" />
+                <span>Sign Out</span>
+              </Menu.Item>
+            </Menu> */}
+          </Authorized>
+          {/* <Flex alignItems="center" component={Link} to={routePaths.signOut.path} className={css['item']}>
+          <Icon type="logout" theme="outlined" />
+          <div>Sign Out</div>
+        </Flex> */}
+        </Flex>
       </Flex>
-    </Flex>
-  );
+    );
+  }
 }
+
+export default withRouter(NavBar);
