2128048ef99db0b15bd2a85d4d83a0256aa6263a
Vladimir Zhukov <vzh.inbox@gmail.com>
2018-09-26 21:43:02 +0600

    Add notifications

--- app/src/components/Page/Page.tsx
+++ app/src/components/Page/Page.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Layout, notification } from 'antd';
 import { Flex, FlexProps } from 'reflexy';
 import { UIStore } from '@vzh/mobx-stores';
 import { RootStore } from 'stores';
 import Loader from 'components/Loader';
 import { observer } from 'mobx-react';
+import { IReactionDisposer, reaction } from 'mobx';
 import NavBar from './NavBar';
 import PageFooter from './PageFooter';
 import css from './Page.css';
@@ -14,29 +15,61 @@ export interface Props extends FlexProps {
   children?: React.ReactNode;
 }
 
-function Page({ uiStore, ...rest }: Props) {
-  const showLoader = uiStore && uiStore.loading;
-  // hide page loader while showing app loader to avoid double loaders
-  // (!store.uiStore.loading || uiStore === store.uiStore);
-  return (
-    <Layout className={css.root}>
-      {showLoader && <Loader />}
+class Page extends React.Component<Props> {
+  private readonly notificationReaction: IReactionDisposer;
 
-      <NavBar />
+  constructor(props: Props) {
+    super(props);
 
-      <Flex
-        column
-        hfill
-        grow
-        shrink={false}
-        component="main"
-        className={css['main-container']}
-        {...rest}
-      />
+    this.notificationReaction = reaction(
+      () => this.props.uiStore && this.props.uiStore.notifications,
+      list =>
+        list &&
+        list.forEach(n => {
+          notification.open({
+            key: n.id.toString(),
+            type: n.type,
+            message: n.text,
+            description: '',
+            duration: 0,
+            onClose: () => {
+              this.props.uiStore && this.props.uiStore.closeNotification(n.id);
+            },
+          });
+        }),
+      { fireImmediately: true }
+    );
+  }
 
-      <PageFooter />
-    </Layout>
-  );
+  componentWillUnmount() {
+    this.notificationReaction();
+  }
+
+  render() {
+    const { uiStore, ...rest } = this.props;
+    const showLoader = uiStore && uiStore.loading;
+    // hide page loader while showing app loader to avoid double loaders
+    // (!store.uiStore.loading || uiStore === store.uiStore);
+    return (
+      <Layout className={css.root}>
+        {showLoader && <Loader />}
+
+        <NavBar />
+
+        <Flex
+          column
+          hfill
+          grow
+          shrink={false}
+          component="main"
+          className={css['main-container']}
+          {...rest}
+        />
+
+        <PageFooter />
+      </Layout>
+    );
+  }
 }
 
 export default observer(Page);
