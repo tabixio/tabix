fac31bfd99105aa8f2fa9ae6575f1920da5e40b4
Igor <isublimity@gmail.com>
2018-10-10 22:40:24 +0300

    dark antD

--- webpack.less-vars.js
+++ webpack.less-vars.js
@@ -2,4 +2,26 @@ export default {
   '@primary-color': '#1DA57A',
   '@link-color': '#1DA57A',
   '@border-radius-base': '2px',
+  '@layout-body-background': '#404040',
+  '@body-background': '#404041',
+  '@layout-sider-background': '#313131',
+  '@component-background': '#313132',
+  '@input-bg': '#313133',
+  '@btn-default-bg': '#262626',
+  '@border-color-base': '#1e1e1e',
+  '@border-color-split': '#262627',
+  '@heading-color': 'fade(#fff, 85%)',
+  '@text-color ': 'fade(#fff, 80%)',
+  '@primary-1': 'fade(#000, 15%)',
+  '@text-color-secondary ': 'fade(#fff, 65%)'
 };
+// @tabs-highlight-color: @primary-color;
+// @tabs-hover-color: @primary-5;
+// @tabs-active-color: @primary-7;
+/*
+need??? global.dark.css
+.ant-tabs.ant-tabs-card > .ant-tabs-bar .ant-tabs-tab {
+  background: #262627;
+}
+
+ */
