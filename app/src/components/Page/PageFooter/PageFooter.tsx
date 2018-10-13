e166e2644d6b069560c580fc050597c5504d8e72
Igor <isublimity@gmail.com>
2018-10-14 00:28:51 +0300

    DropFooter:@todo:rewrite move to menu

--- app/src/components/Page/PageFooter/PageFooter.tsx
+++ app/src/components/Page/PageFooter/PageFooter.tsx
@@ -7,17 +7,18 @@ const version = 1;
 
 export default function PageFooter() {
   return (
-    <Flex component={Layout.Footer} column center className={css.root}>
-      <div>
-        <a href="https://tabix.io/" target="_blank" rel="noopener noreferrer">
-          Tabix.IO
-        </a>{' '}
-        by Tabix LLC © all rights reserved.
-      </div>
-      <div>
-        Build
-        {version}
-      </div>
-    </Flex>
+      <span/>
+    {/*<Flex component={Layout.Footer} column center className={css.root}>*/}
+      {/*<div>*/}
+        {/*<a href="https://tabix.io/" target="_blank" rel="noopener noreferrer">*/}
+          {/*Tabix.IO*/}
+        {/*</a>{' '}*/}
+        {/*by Tabix LLC © all rights reserved.*/}
+      {/*</div>*/}
+      {/*<div>*/}
+        {/*Build*/}
+        {/*{version}*/}
+      {/*</div>*/}
+    // </Flex>
   );
 }
