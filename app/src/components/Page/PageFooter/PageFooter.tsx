dfe526e1982d531ca375dd68b4dace240c3ebc4c
Igor <isublimity@gmail.com>
2018-10-14 00:43:33 +0300

    Footer

--- app/src/components/Page/PageFooter/PageFooter.tsx
+++ app/src/components/Page/PageFooter/PageFooter.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import { Layout } from 'antd';
-import { Flex } from 'reflexy';
-import css from './PageFooter.css';
-
-const version = 1;
+// import { Layout } from 'antd';
+// import { Flex } from 'reflexy';
+// import css from './PageFooter.css';
+// const version = 1;
 
 export default function PageFooter() {
   return (
-      <span/>
-    {/*<Flex component={Layout.Footer} column center className={css.root}>*/}
-      {/*<div>*/}
-        {/*<a href="https://tabix.io/" target="_blank" rel="noopener noreferrer">*/}
-          {/*Tabix.IO*/}
-        {/*</a>{' '}*/}
-        {/*by Tabix LLC © all rights reserved.*/}
-      {/*</div>*/}
-      {/*<div>*/}
-        {/*Build*/}
-        {/*{version}*/}
-      {/*</div>*/}
-    // </Flex>
+    <span/>
   );
 }
+/*
+<Flex component={Layout.Footer} column center className={css.root}>
+    <div>
+        <a href="https://tabix.io/" target="_blank" rel="noopener noreferrer">
+            Tabix.IO
+        </a>{' '}
+        by Tabix LLC © all rights reserved.
+    </div>
+    <div>
+        Build
+        {version}
+    </div>
+</Flex>
+*/
