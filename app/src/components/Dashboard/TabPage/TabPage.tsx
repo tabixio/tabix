4908c9e596639823d046077714dab0aa50c12891
Vladimir Zhukov <vzh.inbox@gmail.com>
2018-10-15 20:41:21 +0600

    Add run code commands processing

--- app/src/components/Dashboard/TabPage/TabPage.tsx
+++ app/src/components/Dashboard/TabPage/TabPage.tsx
@@ -38,7 +38,7 @@ export default class TabPage extends React.Component<Props> {
     onTabModelFieldChange({ name: 'codeEditor', value: Option.of(editor) });
   };
 
-  private onAction = (action: ActionType) => {
+  private onAction = (action: ActionType, eventData?: any) => {
     switch (action) {
       case ActionType.Save: {
         const { store } = this.props;
@@ -49,7 +49,7 @@ export default class TabPage extends React.Component<Props> {
         break;
       case ActionType.RunCurrent: {
         const { store } = this.props;
-        store.execCode();
+        store.execQueries(eventData);
         break;
       }
       case ActionType.RunAll:
@@ -78,10 +78,16 @@ export default class TabPage extends React.Component<Props> {
 
           <Tabs size="small" animated={false} activeKey="draw" className={css.tabs}>
             <Tabs.TabPane key="table" tab="Table view">
-              {model.data.map(data => <DataTable data={data} />).orUndefined()}
+              {/* {model.data.map(data => <DataTable data={data} />).orUndefined()} */}
+              {model.data.map(data => (
+                <DataTable data={data} />
+              ))}
             </Tabs.TabPane>
             <Tabs.TabPane key="draw" tab="Draw view">
-              {model.data.map(data => <Draw data={data} />).orUndefined()}
+              {/* {model.data.map(data => <Draw data={data} />).orUndefined()} */}
+              {model.data.map(data => (
+                <Draw data={data} />
+              ))}
             </Tabs.TabPane>
           </Tabs>
         </Splitter>
