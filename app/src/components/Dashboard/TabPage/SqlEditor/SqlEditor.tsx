9944c37396b57c8f1a4bb0c6d980ac57c480834c
Vladimir Zhukov <vzh.inbox@gmail.com>
2018-11-08 13:10:07 +0600

    Refactor to fix completion on hot reload SqlEditor

--- app/src/components/Dashboard/TabPage/SqlEditor/SqlEditor.tsx
+++ app/src/components/Dashboard/TabPage/SqlEditor/SqlEditor.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 import MonacoEditor from 'react-monaco-editor';
-import monacoEditor, { IRange, Position, Selection, Uri } from 'monaco-editor';
+import monacoEditor, { IRange, Position, Selection } from 'monaco-editor';
 import { Flex, FlexProps } from 'reflexy';
 import classNames from 'classnames';
 import { Omit } from 'typelevel-ts';
-import { ServerStructure } from 'services';
+import { ServerStructure, Query } from 'services';
 import { languageDef, configuration } from './monaco/language/Clickhouse';
 import { themeCobalt } from './monaco/theme/Cobalt';
 import { themeDarcula } from './monaco/theme/Darcula';
 import { themeVsDark } from './monaco/theme/Vsdark';
 import Toolbar, { Props as ToolbarProps, ActionType } from './Toolbar';
+import { globalEditorsMap, getCompletionItems } from './completionItems';
 import css from './SqlEditor.css';
-import { Query } from '../../../../services/api/Query';
-
-const globalMonaco: Monaco = window.monaco;
 
 const monacoEditorOptions: monacoEditor.editor.IEditorConstructionOptions = {
   language: 'clickhouse',
@@ -40,122 +38,20 @@ const monacoEditorOptions: monacoEditor.editor.IEditorConstructionOptions = {
 
 type Monaco = typeof monacoEditor;
 export type CodeEditor = monacoEditor.editor.IStandaloneCodeEditor;
-export type IReadOnlyModel = monacoEditor.editor.IReadOnlyModel;
+
 export interface SqlEditorProps extends Omit<ToolbarProps, 'databases'>, FlexProps {
   content: string;
   onContentChange: (content: string) => void;
   editorRef?: (editor?: CodeEditor) => void;
   serverStructure: ServerStructure.Server;
 }
+
 export interface DatabaseTables {
   table: string;
   db: string;
 }
 
-const modelMap = new WeakMap<Uri, SqlEditor>();
-
-function providerCompletionItems(
-  model: IReadOnlyModel,
-  position: Position
-): Array<monacoEditor.languages.CompletionItem> {
-  const completionItems: Array<monacoEditor.languages.CompletionItem> = [];
-  const sqlEditor = modelMap.get(model.uri);
-  if (
-    !(
-      sqlEditor &&
-      sqlEditor.props &&
-      sqlEditor.props.currentDatabase &&
-      sqlEditor.props.serverStructure
-    )
-  ) {
-    console.error('sqlEditor.props.currentDatabase is empty');
-    console.warn('sqlEditor', sqlEditor);
-    return completionItems;
-  }
-  const selectDB: string = sqlEditor.props.currentDatabase;
-  let currentListTables: Array<DatabaseTables> = [];
-  const queryes: Array<Query> = sqlEditor.tokenizeModel(model, position);
-
-  if (queryes && queryes.length) {
-    // find inCursor=true, fetch all tokens - find  type: "keyword.dbtable.sql" => push to currentListTables
-    currentListTables = [];
-    queryes.forEach((query: Query) => {
-      if (!query.inCursor) return;
-      // @ts-ignore
-      query.tokens.forEach(oToken => {
-        if (['keyword.dbtable.sql', 'keyword.table.sql'].indexOf(oToken.type) !== -1) {
-          let [db, table] = oToken.text
-            .toUpperCase()
-            .trim()
-            .split('.') // @todo : need regexp
-            // @ts-ignore
-            .map(x => x.trim().replace('`', ''));
-
-          if (!table && db) {
-            table = db;
-            db = '';
-          }
-
-          currentListTables.push({ db, table });
-        }
-      });
-    });
-  }
-  // console.log('queryes', queryes);console.log(query.tokens);
-  if (currentListTables.length) console.log('Find database & tables', currentListTables);
-  // add items
-  sqlEditor.props.serverStructure.databases.forEach((db: ServerStructure.Database) => {
-    // Completion:Tables
-    if (!currentListTables.length && db.name !== selectDB) return;
-
-    // for (currentListTables in o ) { if not find return};
-
-    db.tables.forEach((table: ServerStructure.Table) => {
-      table.columns.forEach((column: ServerStructure.Column) => {
-        // console.log('call.providerCompletionItems', selectDB, column);
-        completionItems.push({
-          label: column.name,
-          insertText: `${column.name}`,
-          kind: globalMonaco.languages.CompletionItemKind.Reference,
-          detail: `${column.type} in ${column.table}`,
-          // documentation: table.id,
-        });
-      });
-    });
-  });
-
-  // const textUntilPosition = model.getValueInRange({
-  //     startLineNumber: position.lineNumber,
-  //     startColumn: 1,
-  //     endLineNumber: position.lineNumber,
-  //     endColumn: position.column
-  // });
-  // const [keyword, value] = textUntilPosition.split(':').map(x => x.trim());
-  // const suggestions = keywords.get(keyword);
-  //
-  // if (suggestions) {
-  //     return suggestions.values.map(x => ({
-  //         label: x.name,
-  //         kind: monaco.languages.CompletionItemKind.Enum,
-  //         insertText: ` ${x.name}`,
-  //         documentation: x.description,
-  //         range: {
-  //             startLineNumber: position.lineNumber,
-  //             startColumn: keyword.length + 2,
-  //             endLineNumber: position.lineNumber,
-  //             endColumn: position.column
-  //         }
-  //     }));
-  // }
-  //
-  // return Array.from(keywords.values()).map(property => ({
-  //     label: property.name,
-  //     kind: monaco.languages.CompletionItemKind.Property,
-  //     documentation: property.description,
-  //     insertText: `${property.name}: `
-  // }));
-  return completionItems;
-}
+const globalMonaco: Monaco = window.monaco;
 
 @observer
 export default class SqlEditor extends React.Component<SqlEditorProps> {
@@ -217,7 +113,7 @@ export default class SqlEditor extends React.Component<SqlEditorProps> {
       // registerCompletionItemProvider
       // this.completionItemsDisposable =
       monaco.languages.registerCompletionItemProvider('clickhouse', {
-        provideCompletionItems: providerCompletionItems,
+        provideCompletionItems: getCompletionItems,
       });
       console.log('monaco - register ClickHouseLanguage');
     }
@@ -448,15 +344,15 @@ export default class SqlEditor extends React.Component<SqlEditorProps> {
 
     // Save current component instance to map
     const modelUri = editor.getModel().uri;
-    modelMap.set(modelUri, this);
+    globalEditorsMap.set(modelUri, this);
     // Replace model uri when changed
     editor.onDidChangeModel(({ newModelUrl, oldModelUrl }) => {
-      modelMap.delete(oldModelUrl);
-      modelMap.set(newModelUrl, this);
+      globalEditorsMap.delete(oldModelUrl);
+      globalEditorsMap.set(newModelUrl, this);
     });
     // Clear current component instance from map
     editor.onDidDispose(() => {
-      modelMap.delete(modelUri);
+      globalEditorsMap.delete(modelUri);
     });
     // Bind keys to Editor
     this.bindKeys(editor);
