2ba8970ae900296f810e427381e9f04d8e05eb34
Igor Strykhar <isublimity@gmail.com>
2022-05-07 15:33:03 +0300

    - Up tslib, add antv:g2,s2,AutoChart

--- app/src/components/Dashboard/EditorTabPage/Draw/Draw.tsx
+++ app/src/components/Dashboard/EditorTabPage/Draw/Draw.tsx
@@ -5,6 +5,8 @@ import * as sizeSensor from 'size-sensor';
 import ReactECharts from 'echarts-for-react';
 import { Flex, FlexProps } from 'reflexy';
 import { PlotlyCreator } from 'services';
+import { AutoChart } from '@antv/auto-chart';
+import { Advisor, Linter, ChartAdvisor } from '@antv/chart-advisor';
 
 interface Props {
   data: DataDecorator;
@@ -29,7 +31,7 @@ interface State {
 export default class Draw extends React.Component<Props & FlexProps> {
   private readonly rootRef = React.createRef<HTMLDivElement>();
 
-  private plotRef = React.createRef<Plot>();
+  private chartRef = React.createRef<ReactECharts>();
 
   data: any;
 
@@ -68,49 +70,95 @@ export default class Draw extends React.Component<Props & FlexProps> {
 
   render() {
     const { data, className, ...flexProps } = this.props;
+    const myChartAdvisor = new ChartAdvisor();
+    const ddata = data.rows;
+    console.log('DData', ddata);
+    const results = myChartAdvisor.advise({ data: ddata });
+    console.log('results', results);
+
+    const myAdvisor = new Advisor();
+    const advices = myAdvisor.advise({
+      data: ddata,
+      fields: ['number', 'ss', 'cc'],
+      options: { refine: true },
+    });
+    console.log('advices', advices);
+    const options = {
+      grid: { top: 8, right: 8, bottom: 24, left: 36 },
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series: [
+        {
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+          type: 'line',
+          smooth: true,
+        },
+      ],
+      tooltip: {
+        trigger: 'axis',
+      },
+    };
+    const defaultData = [
+      { price: 100, type: 'A' },
+      { price: 120, type: 'B' },
+      { price: 150, type: 'C' },
+    ];
+    console.log('DATA', data.rows);
     return (
       <Flex componentRef={this.rootRef} column {...flexProps}>
-        <Plot
-          ref={this.plotRef}
-          data={this.data}
-          useResizeHandler
-          debug
-          style={{ width: '100%', height: '100%' }}
-          layout={{
-            font: {
-              size: 12,
-              color: 'white',
-              family: 'Menlo, Inconsolata, Courier New, monospace, Helvetica, arial, sans-serif',
-            },
-
-            plot_bgcolor: '#404041',
-            paper_bgcolor: '#404041',
-            legend: { bgcolor: '#303030', orientation: 'h' },
-            autosize: true,
-
-            // Dark
-            // https://github.com/plotly/dash-technical-charting/blob/master/quantmod/theming/themes.py
-            // https://github.com/plotly/dash-technical-charting/blob/master/quantmod/theming/palettes.py
-            // https://github.com/plotly/plotly.py/blob/master/packages/python/plotly/templategen/definitions.py
-            // colors: {
-            //   increasing: '#00FF00',
-            //   decreasing: '#FF9900',
-            //   border_increasing: 'rgba(255, 255, 255, 0.05)',
-            //   border_decreasing: 'rgba(255, 255, 255, 0.05)',
-            //   primary: '#11AAEE',
-            //   secondary: '#0084FF',
-            //   tertiary: '#FC0D1B',
-            //   quaternary: '#00FF00',
-            //   grey: 'rgba(255, 255, 255, 0.25)',
-            //   grey_light: 'rgba(255, 255, 255, 0.15)',
-            //   grey_strong: 'rgba(255, 255, 255, 0.40)',
-            //   fill: 'rgba(255, 255, 255, 0.10)',
-            //   fill_light: 'rgba(255, 255, 255, 0.05)',
-            //   fill_strong: 'rgba(255, 255, 255, 0.15)',
-            // },
-          }}
-          config={{ responsive: true }}
+        <AutoChart
+          title="CASE 1"
+          description="auto chart analysis"
+          data={data.rows}
+          purpose={'Comparison'}
+          language={'en-US'}
         />
+        {/*<ReactECharts*/}
+        {/*  ref={this.chartRef}*/}
+        {/*  option={options}*/}
+        {/*  notMerge={true}*/}
+        {/*  lazyUpdate={true}*/}
+        {/*  style={{ width: '100%', height: '100%' }}*/}
+        {/*  // layout={{*/}
+        {/*  //   font: {*/}
+        {/*  //     size: 12,*/}
+        {/*  //     color: 'white',*/}
+        {/*  //     family: 'Menlo, Inconsolata, Courier New, monospace, Helvetica, arial, sans-serif',*/}
+        {/*  //   },*/}
+        {/*  //*/}
+        {/*  //   plot_bgcolor: '#404041',*/}
+        {/*  //   paper_bgcolor: '#404041',*/}
+        {/*  //   legend: { bgcolor: '#303030', orientation: 'h' },*/}
+        {/*  //   autosize: true,*/}
+        {/*  //*/}
+        {/*  //   // Dark*/}
+        {/*  //   // https://github.com/plotly/dash-technical-charting/blob/master/quantmod/theming/themes.py*/}
+        {/*  //   // https://github.com/plotly/dash-technical-charting/blob/master/quantmod/theming/palettes.py*/}
+        {/*  //   // https://github.com/plotly/plotly.py/blob/master/packages/python/plotly/templategen/definitions.py*/}
+        {/*  //   // colors: {*/}
+        {/*  //   //   increasing: '#00FF00',*/}
+        {/*  //   //   decreasing: '#FF9900',*/}
+        {/*  //   //   border_increasing: 'rgba(255, 255, 255, 0.05)',*/}
+        {/*  //   //   border_decreasing: 'rgba(255, 255, 255, 0.05)',*/}
+        {/*  //   //   primary: '#11AAEE',*/}
+        {/*  //   //   secondary: '#0084FF',*/}
+        {/*  //   //   tertiary: '#FC0D1B',*/}
+        {/*  //   //   quaternary: '#00FF00',*/}
+        {/*  //   //   grey: 'rgba(255, 255, 255, 0.25)',*/}
+        {/*  //   //   grey_light: 'rgba(255, 255, 255, 0.15)',*/}
+        {/*  //   //   grey_strong: 'rgba(255, 255, 255, 0.40)',*/}
+        {/*  //   //   fill: 'rgba(255, 255, 255, 0.10)',*/}
+        {/*  //   //   fill_light: 'rgba(255, 255, 255, 0.05)',*/}
+        {/*  //   //   fill_strong: 'rgba(255, 255, 255, 0.15)',*/}
+        {/*  //   // },*/}
+        {/*  // }}*/}
+        {/*  // config={{ responsive: true }}*/}
+        {/*/>*/}
       </Flex>
     );
   }
--- app/src/components/Dashboard/ServerOverviewTabPage/ServerOverviewTabPage.tsx
+++ app/src/components/Dashboard/ServerOverviewTabPage/ServerOverviewTabPage.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { SheetComponent } from '@antv/s2-react';
+import '@antv/s2-react/dist/style.min.css';
+import { Lang, setEVALocale } from '@antv/s2';
 
+//
 interface Props {}
 
-export default function ServerOverviewTabPage({  }: Props) {
-  return <div>ServerOverviewTabPage</div>;
+export default function ServerOverviewTabPage({}: Props) {
+  const s2Options = {
+    width: 600,
+    height: 600,
+    lang: 'en_US',
+  };
+  console.log(Lang);
+  setEVALocale('en_US');
+  console.log(Lang);
+  const s2DataConfig = {
+    fields: {
+      //rows: ['a', 'b'],
+      columns: ['a', 'b'],
+      valueInCols: true,
+    },
+    data: [
+      { a: 1, b: 1 },
+      { a: 2, b: 2 },
+      { a: 3, b: 3 },
+    ],
+  };
+
+  return (
+    <div>
+      s2 sd ServerOverviewTabPage
+      <SheetComponent dataCfg={s2DataConfig} options={s2Options} themeCfg={{ name: 'gray' }} />
+    </div>
+  );
 }
